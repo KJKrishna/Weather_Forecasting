a2b3d74ec6fa930bec51f2bbc1109d34ea20185e
Pushti <112201005@smail.iitpkd.ac.in>
2024-06-29 17:15:43 +0530

    Fetch component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Forecast from './components/Forecast/Forecast.jsx'
+
 
 const App = () => {
   return (
     <div>
       Weather Forecasting
+      <Forecast/>
+
     </div>
   )
 }
---
+++ src/components/Forecast/Forecast.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Fetch from './fetch.js'
+
+
+const Forecast = () => {
+    return (
+        <div>
+            Forecast components
+            <Fetch/>
+        </div>
+    );
+};
+
+export default Forecast;
