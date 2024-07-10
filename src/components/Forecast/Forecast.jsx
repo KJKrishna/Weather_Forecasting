a5912b34f551cc74a0e672b47fc004b62b5f72b1
Pushti <112201005@smail.iitpkd.ac.in>
2024-07-11 02:31:23 +0530

    fetch updates

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -6,7 +6,24 @@ const Forecast = () => {
     return (
         <div>
             Forecast components
-            <Fetch/>
+            {/* Use any option line as per requirement
+
+            Search with lat and lon
+
+            <Fetch query="currentWeather" lat={21.170240} lon={72.831062}/>
+            <Fetch query="forecast" lat={21.170240} lon={72.831062} cnt={7} />
+            <Fetch query="airPollution" lat={21.170240} lon={72.831062} />
+
+            */}
+
+            <Fetch city="Surat,Gujarat,India"/>
+            {/*
+            Search with city or state ot country
+
+            <Fetch city="Surat,Gujarat,India"/>
+            <Fetch city="Surat,India"/>
+            <Fetch city="Surat"/>
+                */}
         </div>
     );
 };
