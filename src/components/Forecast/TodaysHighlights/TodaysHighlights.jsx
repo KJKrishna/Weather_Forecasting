f674eec42d3ff76d38cf5e41aa9b21ef6eaac719
GUNDAMARAJU HEMANTH KASHYAP <143811638+Hemanth-Kashyap-Gundamaraju@users.noreply.github.com>
2024-07-17 12:49:21 +0530

    Update TodaysHighlights.jsx

--- src/components/Forecast/TodaysHighlights/TodaysHighlights.jsx
+++ src/components/Forecast/TodaysHighlights/TodaysHighlights.jsx
@@ -7,13 +7,13 @@ function TodaysHighlights() {
     <div>
       <h3>Today's Highlights</h3>
 
-      <Card element='Wind speed' unit='kmph' measurement='7.9'></Card>
-      <Card element='UV Index' unit='UV' measurement='5.50'></Card>
+      <Card element='Wind speed' unit='m/s' measurement='5.3'></Card>
+      <Card element='UV Index' unit='UV' measurement='1'></Card>
       {/* <div className="SunriseSunset">
       <div style={{display:'inline-block'}}><p>Sunrise & Sunset</p><p>sunrise: 6:30AM</p></div>
       <div style={{display:'inline-block',paddingLeft:'10px'}}><p>sunset: 7:02PM</p></div>
     </div> */}
-      <Card element='Sunrize' unit='am' measurement='6:30'></Card>
+      <Card element='Sunrize' unit='am' measurement='6:093'></Card>
       <Card element='Sunset' unit='pm' measurement='7:00'></Card>
 
     </div>
