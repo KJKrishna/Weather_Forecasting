43f1b90c4df656eccfa36fbe4d5226c91e59b919
hemant030406 <112201024@smail.iitpkd.ac.in>
2024-07-18 11:24:24 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About/About'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
 import NavBar from './components/Home/NavBar'
+import Footer from './components/About/Footer'
 const App = () => {
   return (
     <Router>
@@ -13,6 +14,7 @@ const App = () => {
         <Route path='/about' element={<About />} />
         <Route path='/forecast' element={<Forecast/>}/>
       </Routes>
+      <Footer/>
     </Router>
   )
 }
