ea51ee7c0812e73e6e17aa5aa4a06bcb4a6dc2ed
sidarthprabhu <132201015@smail.iitpkd.ac.in>
2024-07-23 12:19:49 +0530

    Icons done

--- src/components/Forecast/CurrWeather/Curr_Weather.jsx
+++ src/components/Forecast/CurrWeather/Curr_Weather.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './styles.css';
 
 const WeatherCard = ({PlaceName, Weather, Temperature, Humidity, WindSpeed, Icon}) => {
-    const weatherIcon = require(`./assets/${Icon}.png`);
     return (
         <div className = "container mt-5">
             <div className='row justify-content-center'>
                 <div className='col-md-8 col-lg-5 WeatherCard text-center' >
                     <h5 className="card-title"> {PlaceName} </h5>
                     <h6 className="card-subtitle mb-2 text-light"> {Weather} </h6>
-                    <img src={weatherIcon} alt="Weather Icon" />
+                    <img 
+                            src={`http://openweathermap.org/img/wn/${Icon}@2x.png`} 
+                            alt={Weather} 
+                        />
                     <p className="card-text">{Temperature}°C</p>
                     <p className="card-text">Humidity: {Humidity}%</p>
                     <p className="card-text">Wind Speed: {WindSpeed} m/s</p>
