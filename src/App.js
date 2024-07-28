fff0cdc3d65b60a1491d05fb67b9c4ec3e81266b
hemant030406 <112201024@smail.iitpkd.ac.in>
2024-07-28 11:17:13 +0530

    lazy and suspense added to prevent delay loading of navbar due to earth bg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import Forecast from './components/Forecast/Forecast'
 import About from './components/About/About'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -8,13 +8,11 @@ import Footer from './components/Footer/Footer'
 const App = () => {
   return (
     <Router>
-      <Suspense>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/forecast' element={<Forecast />} />
         </Routes>
-      </Suspense>
       <Footer />
     </Router>
   )
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Earth from './background'
+import React, { lazy, Suspense } from 'react'
+// import Earth from './background'
 import { Canvas } from '@react-three/fiber'
 import NavBar from '../NavBar/NavBar'
 
+const Earth = lazy(() => import('./background'))
+
 const Home = () => {
   return (
-    <div style={{height:'80%',overflow:'hidden'}}>
-    <NavBar/>
-    <Canvas>
-      <Earth />
-    </Canvas>
+    <div style={{ height: '80%', overflow: 'hidden' }}>
+      <NavBar />
+      <Canvas>
+        <Suspense>
+          <Earth />
+        </Suspense>
+      </Canvas>
     </div>
   )
 }
